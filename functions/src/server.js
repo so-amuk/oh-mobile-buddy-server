d151571406998c04a67f1bce3620d3ba2baef3a8
Amuk Saxena <amuk.saxena@scheduleonce.com>
2020-01-03 18:25:41 +0530

    Updated code for pushing data to firebase database

--- functions/src/server.js
+++ functions/src/server.js
@@ -23,6 +23,28 @@ const headers = {
     'Content-Type': 'application/json'
 };
 
+
+
+// axios.post('https://exp.host/--/api/v2/push/send',
+//         {
+//             "to": "ExponentPushToken[aIEsAvCszTE7AEw66jXtV5]",
+//             "title":"onemore",
+//             "body": "test"
+//         },
+//         {headers: headers}
+//     )
+//     .then((res) => {
+//     console.log(`statusCode: ${res.statusCode}`)
+//     console.log(res)
+//     })
+//     .catch((error) => {
+//     console.error(error)
+//     });
+
+
+
+
+
 /**
 * Reading Value from
 * Firebase Data Object
@@ -35,6 +57,73 @@ app.get("/getusers", (req,res) => {
 });
 
 
+
+
+app.get("/senddata", (req,res) => {
+    axios.post('https://exp.host/--/api/v2/push/send', {headers: headers}, [
+        {
+            "to": "ExponentPushToken[aIEsAvCszTE7AEw66jXtV5]",
+            "title":"hello",
+            "body": "world"
+        }
+    ])
+    .then((res) => {
+    console.log(`statusCode: ${res.statusCode}`)
+    console.log(res)
+    })
+    .catch((error) => {
+    console.error(error)
+    });
+    res.send("Received POST request!");
+});
+
+
+/**
+* Loading Firebase Database and refering 
+* to user_data Object from the Database
+*/
+app.get("/pushusers", (req,res) => {
+
+    /**
+    * Setting Data Object Value
+    */
+    ref.set(
+        [
+            {"WWWWFqYXRkZXZ2QGdtYWlsLmNvbQ253D25EE": "ExponentPushToken[aIEsAvCszTE7AEw66jXtV5] "},
+            {"ZZZZFqYXRkZXZ2QGdtYWlsLmNvbQ253D2WWW": "ExponentPushToken[aIEsAvCszTE7AEw66jXtV5] "},
+            {"QQQQFqYXRkZXZ2QGdtYWlsLmNvbQ253D25EE": "ExponentPushToken[aIEsAvCszTE7AEw66jXtV5] "},
+            {"AAAAFqYXRkZXZ2QGdtYWlsLmNvbQ253D2WWW": "ExponentPushToken[aIEsAvCszTE7AEw66jXtV5] "}
+        ]
+        );
+
+    res.status(200).send([
+        {"WWWWFqYXRkZXZ2QGdtYWlsLmNvbQ253D25EE": "ExponentPushToken[aIEsAvCszTE7AEw66jXtV5] "},
+        {"ZZZZFqYXRkZXZ2QGdtYWlsLmNvbQ253D2WWW": "ExponentPushToken[aIEsAvCszTE7AEw66jXtV5] "},
+        {"QQQQFqYXRkZXZ2QGdtYWlsLmNvbQ253D25EE": "ExponentPushToken[aIEsAvCszTE7AEw66jXtV5] "},
+        {"AAAAFqYXRkZXZ2QGdtYWlsLmNvbQ253D2WWW": "ExponentPushToken[aIEsAvCszTE7AEw66jXtV5] "}
+    ]);    
+}
+);
+
+app.get('/test', (req,res)=>{
+    fetch('https://exp.host/--/api/v2/push/send', {
+      body: JSON.stringify({
+        to: 'ExponentPushToken[FQp1aQMsoa5oH63DhutEwj]',
+        title: 'title',
+        body: 'body',
+        data: { message: `title-body` },
+        sound: "default"
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      method: 'POST',
+    });
+
+
+
+      res.status(200).send({status: "true"});
+});
 
 app.post("/notification", (req, res) => {
     ref.once("value", function (snapshot) {
@@ -49,20 +138,23 @@ app.post("/notification", (req, res) => {
         axios.post('https://exp.host/--/api/v2/push/send',
             {
                 "to": data[Buffer.from(req.body.UserEmail).toString('base64')],
-                "title": "onemore",
-                "body": JSON.stringify(dbObject)
+                "title": dbObject.meetingData.subject,
+                "body": `Starting at ${dbObject.meetingData.meetingDate} UTC`
             },
             { headers: headers }
         )
-            .then((res) => {
+            .then((resp) => {
                 console.log('notification ended');
                 notificationref.push({ meetingData: dbObject.meetingData });
+                res.status(200).send({"status": "OK"});
+                console.log(resp)
             })
             .catch((error) => {
                 console.log('notification error');
-                console.error(error);
+                console.error(error)
+                res.status(400).send({"status": "bad request"});
             });
-        res.status(200).send(dbObject);
+       
     });
 });
 
