93a28d7ed648a64bdb006b27db11c4b457b40f9c
Amuk Saxena <amuk.saxena@scheduleonce.com>
2020-01-03 18:25:29 +0530

    Updated code for pushing data to firebase dataabse

--- functions/src/server.js
+++ functions/src/server.js
@@ -12,8 +12,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Loading Firebase Package
 var firebase = require("firebase");
 firebase.initializeApp({
-    serviceAccount: "oh-mobile-buddy-firebase-adminsdk-foumu-ff60f36ddf.json",
-    databaseURL: "https://oh-mobile-buddy.firebaseio.com"
+    serviceAccount: "oh-mobile-buddy-bc7a9-firebase-adminsdk-2mtsn-ab256f7bda.json",
+    databaseURL: "https://oh-mobile-buddy-bc7a9.firebaseio.com"
   });  //by adding your credentials, you get authorized to read and write from the database
 var db = firebase.database();
 var ref = db.ref("users");
@@ -22,28 +22,6 @@ var ref = db.ref("users");
 const headers = {
     'Content-Type': 'application/json'
 };
-
-
-
-// axios.post('https://exp.host/--/api/v2/push/send',
-//         {
-//             "to": "ExponentPushToken[aIEsAvCszTE7AEw66jXtV5]",
-//             "title":"onemore",
-//             "body": "test"
-//         },
-//         {headers: headers}
-//     )
-//     .then((res) => {
-//     console.log(`statusCode: ${res.statusCode}`)
-//     console.log(res)
-//     })
-//     .catch((error) => {
-//     console.error(error)
-//     });
-
-
-
-
 
 /**
 * Reading Value from
@@ -57,73 +35,6 @@ app.get("/getusers", (req,res) => {
 });
 
 
-
-
-app.get("/senddata", (req,res) => {
-    axios.post('https://exp.host/--/api/v2/push/send', {headers: headers}, [
-        {
-            "to": "ExponentPushToken[aIEsAvCszTE7AEw66jXtV5]",
-            "title":"hello",
-            "body": "world"
-        }
-    ])
-    .then((res) => {
-    console.log(`statusCode: ${res.statusCode}`)
-    console.log(res)
-    })
-    .catch((error) => {
-    console.error(error)
-    });
-    res.send("Received POST request!");
-});
-
-
-/**
-* Loading Firebase Database and refering 
-* to user_data Object from the Database
-*/
-app.get("/pushusers", (req,res) => {
-
-    /**
-    * Setting Data Object Value
-    */
-    ref.set(
-        [
-            {"WWWWFqYXRkZXZ2QGdtYWlsLmNvbQ253D25EE": "ExponentPushToken[aIEsAvCszTE7AEw66jXtV5] "},
-            {"ZZZZFqYXRkZXZ2QGdtYWlsLmNvbQ253D2WWW": "ExponentPushToken[aIEsAvCszTE7AEw66jXtV5] "},
-            {"QQQQFqYXRkZXZ2QGdtYWlsLmNvbQ253D25EE": "ExponentPushToken[aIEsAvCszTE7AEw66jXtV5] "},
-            {"AAAAFqYXRkZXZ2QGdtYWlsLmNvbQ253D2WWW": "ExponentPushToken[aIEsAvCszTE7AEw66jXtV5] "}
-        ]
-        );
-
-    res.status(200).send([
-        {"WWWWFqYXRkZXZ2QGdtYWlsLmNvbQ253D25EE": "ExponentPushToken[aIEsAvCszTE7AEw66jXtV5] "},
-        {"ZZZZFqYXRkZXZ2QGdtYWlsLmNvbQ253D2WWW": "ExponentPushToken[aIEsAvCszTE7AEw66jXtV5] "},
-        {"QQQQFqYXRkZXZ2QGdtYWlsLmNvbQ253D25EE": "ExponentPushToken[aIEsAvCszTE7AEw66jXtV5] "},
-        {"AAAAFqYXRkZXZ2QGdtYWlsLmNvbQ253D2WWW": "ExponentPushToken[aIEsAvCszTE7AEw66jXtV5] "}
-    ]);    
-}
-);
-
-app.get('/test', (req,res)=>{
-    fetch('https://exp.host/--/api/v2/push/send', {
-      body: JSON.stringify({
-        to: 'ExponentPushToken[FQp1aQMsoa5oH63DhutEwj]',
-        title: 'title',
-        body: 'body',
-        data: { message: `title-body` },
-        sound: "default"
-      }),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      method: 'POST',
-    });
-
-
-
-      res.status(200).send({status: "true"});
-});
 
 app.post("/notification", (req, res) => {
     ref.once("value", function (snapshot) {
@@ -146,11 +57,10 @@ app.post("/notification", (req, res) => {
             .then((res) => {
                 console.log('notification ended');
                 notificationref.push({ meetingData: dbObject.meetingData });
-                console.log(res)
             })
             .catch((error) => {
                 console.log('notification error');
-                console.error(error)
+                console.error(error);
             });
         res.status(200).send(dbObject);
     });
